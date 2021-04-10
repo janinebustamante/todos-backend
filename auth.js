3aa1bbb0065d42293d318cc8c60944ab017f9bd2
Janine Roxanne Bustamante <janineroxanne.bustamante@gmail.com>
2021-04-10 20:41:17 +0800

    get userId from accessToken

--- auth.js
+++ auth.js
@@ -4,3 +4,29 @@ const secret = process.env.JWT_SECRET;
 module.exports.createAccessToken = (payload) => {
     return jwt.sign(payload, secret)
 }
+
+//middleware to identify if the request contains a valid authorization header
+module.exports.verify = (req, res, next) => {
+    let token = req.headers.authorization;
+
+    if (typeof token !== 'undefined') {
+        token = token.slice(7, token.length)
+
+        return jwt.verify(token, secret, (err, data) => {
+            return (err) ? res.send({ auth: 'failed' }) : next();
+        })
+    } else {
+        return res.send({ auth: 'failed' });
+    }
+}
+
+//decode function return jwt payload
+module.exports.decode = (token) => {
+    if (typeof token !== 'undefined') {
+        token = token.slice(7, token.length)
+
+        return jwt.verify(token, secret, (err, data) => {
+            return (err) ? null : jwt.decode(token, { complete: true }).payload
+        })
+    }
+}
--- routes/todos.js
+++ routes/todos.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const TodoController = require('../controllers/TodoController');
+const auth = require('../auth');
 
-router.post('/', async (req, res) => {
+router.post('/', auth.verify, async (req, res) => {
     const { text } = req.body;
-    res.send(`Hello ${text}`)
+    const token = req.headers.authorization;
+    const payload = auth.decode(token);
+    const userId = payload.id;
+    res.send(`Hello ${text}, userid = ${userId}`)
 })
 
 
