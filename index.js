491f06d7a372333d6266fefa0ced92ae7e337822
Janine Roxanne Bustamante <janineroxanne.bustamante@gmail.com>
2021-04-10 15:46:41 +0800

    initial express.js setup

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose'); //connects mongodb
+const cors = require('cors');
+require('dotenv').config();
+
+app.use(cors());
+
+mongoose.connection.once('open', () => console.log('Connected to MongoDB Atlas'));
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+})
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`API is now online on port ${port}`);
+}) 
